d5cd24856f240d227e06f1f66e6fc0175ef5d4e6
thisiskeshavyadavv@gmail.com <Keshav yadav>
2024-09-25 22:38:32 +0530

    json in view and component importing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,50 @@
-import './App.css'
 
-function App() {
-  return (
-    <>
-      <h1>get start</h1>
-    </>
-  )
-}
 
-export default App
+
+// day 1 and 2 
+// import './App.css'
+// import About from './Components/About'
+// import Nav from './Components/Nav'
+// import Home from './Components/Home'
+
+// function App() {
+
+//   let details = [
+//     {
+//       name: "liger",
+//       native:"africa",
+//       status: "in-progress",
+//     },
+//     {
+//       name: "tiger",
+//       native:"indonesia",
+//       status: "active"
+//     },
+//     {
+//       name: "lion",
+//       native:"India",
+//       status: "active"
+//     }
+//   ]
+//   let x = details.map((item,index)=>{
+//         return (
+//           <div key = {index}>
+//             <h1>name : {item.name}</h1>
+//             <h1>native : {item.native}</h1>
+//             <h1>status : {item.status}</h1>
+//           </div>
+//         )
+//   })
+
+//   return (
+//     <>
+//       <h1>{x}</h1>
+//       <h1 >this is react vite boilerplate {import.meta.env.VITE_API}</h1> 
+//       <About/>
+//       <Home/>
+//       <Nav/>
+//     </>
+//   )
+// }
+
+// export default App
