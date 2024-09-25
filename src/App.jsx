5c966afcea2ed6f142643015f2fff052686ac731
thisiskeshavyadavv@gmail.com <Keshav yadav>
2024-09-25 23:28:46 +0530

    useState day 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import './App.css'
-import React from 'react'
-
-  let info = () =>{
-    console.log("hello virendra")
-  }
-  let advanced = (data) =>{
-    console.log(data)
-  }
-
+// day 4 useState 
+import './index.css'
+import React, { useState } from 'react'
+// const export and {} hai and name likha hai
 const App = () => {
+
+  const [title, settitle] = useState("i am learning backend...")
+  // let title = "i am learning backend..."
+
+  const change = ()=>{
+    // title = "now learning React..."
+    settitle("now learning React...")
+    console.log(title)
+  }
+
   return (
     <div>
-      <button onClick={info} className='bg-red-600 rounded py-2 px-5' >hello to virendra</button>
-      <button onClick={()=>advanced("kaise ho adars")} className='bg-red-600 rounded py-2 px-5' >hello to adarsh</button>
+      <h1>{title}</h1>
+      <button onClick={()=>change()} className='px-5 py-2 bg-slate-600 m-5' >change course</button>
     </div>
   )
 }
@@ -20,9 +24,33 @@ const App = () => {
 export default App
 
 
+// day 3 function in react 
+
+// import './App.css'
+// import React from 'react'
+
+//   let info = () =>{
+//     console.log("hello virendra")
+//   }
+//   let advanced = (data) =>{
+//     console.log(data)
+//   }
+
+// const App = () => {
+//   return (
+//     <div>
+//       <button onClick={info} className='bg-red-600 rounded py-2 px-5' >hello to virendra</button>
+//       <button onClick={()=>advanced("kaise ho adars")} className='bg-red-600 rounded py-2 px-5' >hello to adarsh</button>
+//     </div>
+//   )
+// }
+
+// export default App
 
 
-// day 1 and 2 
+
+
+// day 1 and 2 json in view {browser} and Component
 // import './App.css'
 // import About from './Components/About'
 // import Nav from './Components/Nav'
